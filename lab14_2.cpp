9b36870abc151f038f1fa381f9ea08caafd051d1
Jjjjaoant <thanchanok_naensin@cmu.ac.th>
2024-01-05 16:30:21 +0700

    complete

--- lab14_2.cpp
+++ lab14_2.cpp
@@ -9,6 +9,8 @@ void findLocalMax(const double [][N], bool [][N]);
 
 void showMatrix(const bool [][N]);
 
+
+
 int main(){
 	double A[N][N]; 
 	bool B[N][N]; 
@@ -18,4 +20,36 @@ int main(){
 	cout << "Result is \n";
 	showMatrix(B);
 	return 0;
+}
+
+void inputMatrix(double A[][N]){
+   for(int a = 0 ; a < N ; a++){
+    cout << "Row " << a+1 <<": ";
+        for(int b = 0 ; b < N ; b++){
+        cin >> A[a][b]  ;
+        }
+    }
+}
+
+void findLocalMax(const double input[][N], bool output[][N]){
+		for(int i = 0;i < N ;i++){
+			for(int j = 0 ; j < N ; j++){
+				if(i == 0 || j == 0 || j == N-1 || i == N-1){
+					output[i][j] = 0;
+				}
+				else if( input[i][j] >= input[i-1][j] && input[i][j] >= input[i+1][j] && input[i][j]  >= input[i][j+1] && input[i][j] >= input[i][j-1] ){
+					output[i][j] = 1;
+				}
+				else output[i][j] = 0;
+			}
+		}
+}
+
+void showMatrix(const bool A[][N]){
+    for(int i = 0 ; i < N ; i++){
+        for(int j = 0 ; j < N ; j++){
+        cout << A[i][j] << " " ;
+        }
+    cout << "\n" ;
+    }
 }
